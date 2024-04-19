ed1cd1c63a44f12476d4ee65bcc6b97e7fa10edf
SantiAsenzoChancalay <mateoasenzo02@gmail.com>
2024-04-19 00:12:41 -0300

    cambio nombres

--- BackEnd/ByPass/ByPass/tests/test_user.py
+++ BackEnd/ByPass/ByPass/tests/test_user.py
@@ -6,4 +6,4 @@ from apps.event.models import Event
 def test_event_creation(self):
     user= User.objects.first()
     self.client.force_login(user)
-    evento_dict= {'titulo':
+    #evento_dict= {'titulo':
