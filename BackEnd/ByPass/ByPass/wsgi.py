7a2ad66d8f00c06d359ce2552a4c5b6952dbfd07
lucaslatessa <lucaslatessa@gmail.com>
2024-04-19 00:24:22 -0300

    actualizacion

--- BackEnd/ByPass/ByPass/wsgi.py
+++ BackEnd/ByPass/ByPass/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for webappexample project.
+WSGI config for ByPass project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webappexample.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ByPass.settings')
 
 application = get_wsgi_application()
