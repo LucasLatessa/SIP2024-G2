39a9400a9cf5b245d84c89cda1213a6417157609
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-04-20 00:02:57 -0300

    Creacion backend que maneja Roles y Permisos

--- BackEnd/ByPass/evento/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('crear_evento/', views.crear_evento, name='crear_evento'),
-]
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,9 @@ function App() {
         <LogoutButton/>
         <SignupButton/>
         <Profile/>
+        <Switch>
+          
+        </Switch>
     </div>
   );
 }
