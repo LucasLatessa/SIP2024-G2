dc833c1c188dddf85bc67f8c1ab0ea0e14f665e3
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-04-19 22:59:49 -0300

    FrontEnd hace login, logout y register

---
+++ frontend/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        audience:audience,
+        redirect_uri: redirectUri,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
