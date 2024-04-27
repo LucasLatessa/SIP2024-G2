368172c15f57d9a1d99d64a50076de29743596e9
lucaslatessa <lucaslatessa@gmail.com>
2024-04-27 15:59:41 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './components/home';
 import { Profile } from './components/profile';
-
 function App() {
-  const { isAuthenticated } = useAuth0();
-
   return (
     <Router>
       <Routes>
