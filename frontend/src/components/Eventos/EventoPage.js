e9a3fdc966377b3b43f9cb4cdc1ecd6da96da56f
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-05-10 17:34:08 -0300

    Estilos comprar entrada

--- frontend/src/components/Eventos/EventoPage.js
+++ frontend/src/components/Eventos/EventoPage.js
@@ -7,34 +7,38 @@ import {
   getEvento,
 } from "../../services/eventos.service";
 import "../styles/EventoPage.css";
-import { useSearchParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
 
 //Pagina donde se mostrara datos del evento y la posibilidad de comprar entradas
 export const EventoPage = () => {
   const { id } = useParams(); //Obtengo el id del evento
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
   const [eventos, setEventos] = useState([]);
   const [tickets, setTickets] = useState([]);
 
-  //Realizo la peticion para obtener el evento y los tickets disponibles
+  //Realizo la peticion para obtener el evento
   useEffect(() => {
     async function cargarEventos() {
       const resEvento = await getEvento(id); //Solo eventos validos, si no existe hay que arreglarlo
       setEventos(resEvento.data);
     }
-    
-    async function cargarTicketsDelEvento(){
-      const resTicket = await getTicketByEvent(id);
-      setTickets(resTicket.data)
-    }
-
     cargarEventos();
   }, []);
+
+  /*Compra de tickets*/
+  const onSubmit = handleSubmit(async (data) => {
+    //const res = await comprarTicket(data);
+  });
 
   return (
     <>
       <Header />
-      <main className="App">
+      <main className="App eventoPage">
         {eventos ? ( // Si existe el evento muestro los datos
           <>
             <header className="headerEvento">
@@ -45,21 +49,54 @@ export const EventoPage = () => {
               />
             </header>
             <article className="informacionEvento">
-              <h1 className="titulo">{eventos.nombre}</h1>
-              <p className="fecha">
-                Fecha del evento: {eventos.fecha} - {eventos.hora}
-              </p>
+              <section className="titulo-fecha">
+                <h1 className="titulo">{eventos.nombre}</h1>
+                <p className="fecha">
+                  Fecha del evento: {eventos.fecha} - {eventos.hora}
+                </p>
+              </section>
+
               <section className="comprarEntrada">
                 <h3>Compra tu entrada</h3>
-                <form>
-                  { /* COMPRAR ENTRADA FORM */}
+                <form className="formComprarEntrada">
+                  {/* COMPRAR ENTRADA FORM */}
+                  <label htmlFor=""> Tipo de entrada
+                    <select
+                      {...register("tipoEntrada", {
+                        required: true,
+                      })}
+                    >
+                      <option value="STANDARD">STANDARD</option>
+                      <option value="PLATINIUM">PLATINIUM</option>
+                      <option value="VIP">VIP</option>
+                    </select>
+                  </label>
+
+                  <label>
+                    Cantidad Entrada
+                    <input
+                      type="number"
+                      {...register("cantidadEntradas", {
+                        required: true,
+                      })}
+                    />
+                  </label>
+                  <div className="comprarTicketSubmit">
+                    <input type="submit" value="Comprar" />
+                  </div>
+                  
                 </form>
               </section>
-              <section>
+              <section className="acercaDelEvento">
                 <h3>Acerca del evento</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam totam ut ipsum obcaecati neque voluptate labore nesciunt eligendi ab mollitia. Ipsa, culpa voluptatibus? Repudiandae minus corporis, ab ipsam eum est?</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+                  totam ut ipsum obcaecati neque voluptate labore nesciunt
+                  eligendi ab mollitia. Ipsa, culpa voluptatibus? Repudiandae
+                  minus corporis, ab ipsam eum est?
+                </p>
               </section>
-              <section>
+              <section className="comoLLegar">
                 <h3>Como llegar</h3>
                 {/* Mapita de Google Maps*/}
               </section>
