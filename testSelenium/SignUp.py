6beb96d279539045d83a268a3f688de1fbdcfc18
lucaslatessa <lucaslatessa@gmail.com>
2024-05-18 16:28:00 -0300

    Terminado Test SignUp

--- testSelenium/SignUp.py
+++ testSelenium/SignUp.py
@@ -4,48 +4,58 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 import time
 
-def ejecutarClick(driver, selector):
-    try:
-        # Espera hasta que el elemento sea clicable y desplázate hacia él
-        elemento = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, selector)))
-        driver.execute_script("arguments[0].scrollIntoView();", elemento)
-        elemento.click()
-        print(f"Clicked on element with selector: {selector}")
-    except Exception as e:
-        print(f"Failed to click on element with selector: {selector}")
-        print(e)
+def ejecutarClick(selector):
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, selector))).click()
 
-def modificarCampo(driver, nombre, selector):
-    try:
-        campo = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, selector)))
-        campo.clear()
-        campo.send_keys(nombre)
-        print(f"Modified field with selector: {selector} with value: {nombre}")
-    except Exception as e:
-        print(f"Failed to modify field with selector: {selector}")
-        print(e)
+def modificarCampo(nombre,selector):
+    campo = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, selector)))
+    campo.clear()
+    campo.send_keys(nombre)
 
-usuario = "pepito123@gmail.com"
-contraseña = "pepito123"
+usuario = "pepito123456@gmail.com"
+contraseña = "Pepito123456"
+
+# Configuración para usar Brave
+brave_path = "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
+options = webdriver.ChromeOptions()
+options.binary_location = brave_path
+
+# Crear el driver de Brave
+driver = webdriver.Chrome(options=options)
 
 # Abrir página
-driver = webdriver.Chrome()
 driver.get("http://localhost:4040/")
 
 # Inicio sesion
-ejecutarClick(driver, "div.login")
+ejecutarClick("div.login")
 
-# Espera explícita para asegurar que el registro link está disponible
-#NO HACE EL CLICK, EL SELECTOR ESTA BIENN
-ejecutarClick(driver, "a.c8861f699.c24d8133e")
+time.sleep(2)
+
+# Registrese
+ejecutarClick("a[href*='u/signup']")
 
 # Correo
-modificarCampo(driver, usuario, "input[name='email']")
+modificarCampo(usuario, "input[name='email']")
 
 # Contraseña
-modificarCampo(driver, contraseña, "input[name='password']")
+modificarCampo(contraseña, "input[name='password']")
 
-time.sleep(10)
+time.sleep(2)
 
-# Cerrar el navegador
-driver.quit()
+#IniciarSesion/Registrarse
+ejecutarClick("button[type='submit']")
+
+#Autorizar aplicación
+ejecutarClick("button[type='submit']")
+
+time.sleep(3)
+
+# Hacer clic en el checkbox de "Acepto los Términos y Condiciones"
+ejecutarClick("label input[type='checkbox']")
+
+time.sleep(2)
+
+# Hacer clic en el botón de "Crear Cuenta"
+ejecutarClick("button") 
+
+time.sleep(10000)
