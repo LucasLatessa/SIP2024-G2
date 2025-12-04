b203e9ac62f0513c356054c9460732367fe38d2f
Josue Gatica Odato <josuegaticaodato@gmail.com>
2025-12-04 19:43:40 -0300

    Informacion de cuenta y logout

--- frontend-react/src/components/Profile/Common/AccountInfo.jsx
+++ frontend-react/src/components/Profile/Common/AccountInfo.jsx
@@ -1,40 +1,29 @@
+import { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import styles from "./AccountInfo.module.css";
 
-export default function AccountInfo ({ user, role }){
-  // return (
-  //   <section className="account-info">
-  //     <div className="account-card">
-  //       <img
-  //         src={user.picture}
-  //         alt="profile"
-  //         className="account-avatar"
-  //       />
+export default function AccountInfo({ usuario, photo, role }) {
+  const { logout } = useAuth0();
 
-  //       <div className="account-data">
-  //         <h2 className="account-name">{user.name}</h2>
-  //         <p className="account-email">{user.email}</p>
-  //         <span className="account-role">
-  //           {role === "customer" && "Cliente"}
-  //           {role === "producer" && "Productora"}
-  //           {role === "admin" && "Administrador"}
-  //         </span>
-  //       </div>
-  //     </div>
-  //   </section>
-  // );
-
-  console.log(user);
-
-  return(
+  return (
     <section className={styles.accountInfo}>
       <h1> Informacion de cuenta </h1>
       <hr />
       <article className={styles.userAccountInfo}>
-        <h3>{user.nombre}</h3>
-        <p></p>
-        <img src="" alt="" />
+        <div>
+          <h3>Hola, {usuario?.nombre}</h3>
+          <p>{usuario?.rol}</p>
+        </div>
+        <img src={photo} alt="Foto de perfil" />
       </article>
-      
+      <button
+        className={styles.logOut}
+        onClick={() =>
+          logout({ logoutParams: { returnTo: window.location.origin } })
+        }
+      >
+        Cerrar sesion
+      </button>
     </section>
   );
 }
--- frontend-react/src/pages/Profile/Profile.jsx
+++ frontend-react/src/pages/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import AccountInfo from "../../components/Profile/Common/AccountInfo"
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useUserRole } from "../../utils/useUserRole"
 import { useNavigate } from "react-router-dom";
@@ -10,12 +10,35 @@ import styles from "./Profile.module.css";
 
 export default function Profile(){
   const { user, logout, isAuthenticated } = useAuth0();
+  const [usuarioData, setusuarioData] = useState(null);
   const role = useUserRole(usuarioData);
+
+  //Traigo los datos del cliente cuando se renderiza el componente
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await getUserNick(user.nickname);
+        setusuarioData(response.data.usuario);
+      } catch (error)
+        {
+          console.error("Error al recuperar los datos del usuario:", error);
+        }
+    };
+
+    if (isAuthenticated) {
+      fetchUserData();
+    }
+  }, [isAuthenticated, user?.nickname]);
+  
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
 
   return (
     <main className={styles.profile}>
-      <NavProfile user={user} role={role} />
-      <AccountInfo user={user} role={role} />
+      <NavProfile usuario={usuarioData} role={role} />
+      <AccountInfo usuario={usuarioData} photo={user?.picture} role={role} />
     </main>
   );
 }
