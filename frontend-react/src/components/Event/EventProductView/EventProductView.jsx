403a450d32a339e17b6abd861dbec90e6f5e6d71
Josue Gatica Odato <josuegaticaodato@gmail.com>
2025-12-17 02:19:50 -0300

    Ver mis eventos como productora

---
+++ frontend-react/src/components/Event/EventProductView/EventProductView.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./EventProductView.module.css";
+
+export const EventProductView = ({ id, nombre, imagen }) => {
+  return (
+    <article className={styles.eventoProductora}>
+      <h3 className={styles.titulo}>{nombre}</h3>
+
+      <img
+        src={imagen}
+        alt={`Imagen del evento ${nombre}`}
+        className={styles.imagen}
+      />
+
+      <div className={styles.acciones}>
+        <Link
+          to={`/perfil/productora-eventos/modificar/${id}`}
+          className={styles.modificar}
+        >
+          Modificar datos
+        </Link>
+
+        <Link
+          to={`/perfil/productora-eventos/validar/${id}`}
+          className={styles.validar}
+        >
+          Validar entrada
+        </Link>
+
+        <Link
+          to={`/perfil/productora-eventos/reporte/${id}`}
+          className={styles.reporte}
+        >
+          Ver reporte
+        </Link>
+      </div>
+
+      
+    </article>
+  );
+};
--- frontend-react/src/pages/Profile/Productora/ProductEventos/ProductEventos.jsx
+++ frontend-react/src/pages/Profile/Productora/ProductEventos/ProductEventos.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { getEventosByProductora } from "../../../../services/eventos.service";
+import "./ProductEventos.css";
+import { EventProductView } from "../../../../components/Event/EventProductView/EventProductView";
 
 function ProductEventos() {
+  const [eventos, setEventos] = useState([]);
+  const { usuario, role, photo } = useOutletContext();
+
+  useEffect(() => {
+      const fetchEventos = async () => {
+      try {
+        //console.log(id)
+        const response = await getEventosByProductora(usuario.user_id);
+        setEventos(response.data);
+      } catch (error) {
+        console.error("Error al obtener la lista de eventos de la productora");
+      }}
+
+      fetchEventos();
+  }, [role])
+
+  useEffect(() => {
+    console.log(eventos)
+  },[eventos])
+
   return (
-    <div>ProductEventos</div>
-  )
+    <>
+      <article className="productEventos">
+        <h1>Mis Eventos</h1>
+        <hr />
+        <section className="allListaEventosProductora">
+          {eventos?.map((evento) => (
+            <EventProductView
+              key={evento.id_Evento}
+              id={evento.id_Evento}
+              nombre={evento.nombre}
+              imagen={evento.imagen}
+            />
+          ))}
+        </section>
+        <Link className="crearEventoProductora" to="/perfil/productora-eventos/crear">
+        Crear Evento
+        </Link>
+      </article>
+    </>
+    )
 }
 
 export default ProductEventos
